f0dcb25f9f10abc703d6232a333e0b79dcd7c92c
Mickey <braillejapheth@gmail.com>
2025-07-07 13:55:34 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "RookieTL Bot",
   botOwner: 'Rookie', // Your name
   ownerNumber: '254784522401', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
